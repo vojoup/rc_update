540ef26c6564287e93d9707574ff285066a674ea
Vojta Oupicky <voj.oup@gmail.com>
2017-07-24 17:06:34 +0200

    Now does cd
    If a file is modified, you get cded in the RCs dir.

--- rc_update.sh
+++ rc_update.sh
@@ -6,9 +6,11 @@ difvim=`diff -n ~/.vimrc ~/RCs/vimrc`
 if [ -n "$difbash" ] 
     then cp ~/.bashrc ~/RCs/bashrc  
     echo "bashrc pozmeneno"
+    cd ~/RCs
 fi
 
 if [ -n "$difvim" ] 
     then cp ~/.vimrc ~/RCs/vimrc  
     echo "vimrc pozmeneno"
+    cd ~/RCs
 fi
